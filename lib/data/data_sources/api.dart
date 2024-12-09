08555b0906c3292ec4769672d4e9483ed42e432c
Daffa Aditya Rejasa Ruswanto <e41211358@student.polije.ac.id>
2024-12-09 17:48:54 +0700

    update api

--- lib/data/data_sources/api.dart
+++ lib/data/data_sources/api.dart
@@ -1,5 +1,5 @@
 String http = 'https://antreeze.kingperseus.online';
-// String http = 'http://192.168.0.106:8000';
+// String http = 'http://192.168.119.10:8000';
 
 String apiService = '$http/api/';
 String apiImage = '$http/storage/layanan/';
